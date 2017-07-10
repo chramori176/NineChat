f1834a7a5e15408827766d9826bb24c2a39563b1
Brian Schmalz <bschmalz@Brians-MacBook-Air.local>
2017-07-10 12:33:14 -0700

    Got this setup with our server.

--- server/model/users.js
+++ server/model/users.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-mongoose.connect('mongodb://catlover:ilovetesting@ds151222.mlab.com:51222/ninechat');
+//mongoose.connect('mongodb://catlover:ilovetesting@ds151222.mlab.com:51222/ninechat');
 mongoose.connection.once('open', () => {
   console.log('Connected with MongoDB ORM - mongodb-orm');
 });
