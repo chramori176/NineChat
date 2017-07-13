dc13d17464955ff7a55f07831f2998c1d808d90e
Chris Amori <chris.amori93@gmail.com>
2017-07-12 18:27:08 -0700

    Fix message property from message to content

--- public/client/components/Chatbox.js
+++ public/client/components/Chatbox.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 
 function Chatbox(props) {
+  console.log(props.messages);
   if (props.messages.length > 0) {
     const msgs = props.messages.map((message, i) => {
       return (
         <div key={message._id} className="msgcontainer">
           <div className="msgname">{message.src}:</div>
-          <div className="msg">{message.message}</div>
+          <div className="msg">{message.content}</div>
         </div>
       )
     });
